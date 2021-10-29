1bdabdc3f1b0f52b386e2c9b35c756c5e2474437
webPriya <priyachouhan1019@gmail.com>
2021-10-28 18:53:09 -0700

    test case for password rule 4

--- src/test/java/com/bdlz/JunitRegistration/JunitUserRegistrationTest.java
+++ src/test/java/com/bdlz/JunitRegistration/JunitUserRegistrationTest.java
@@ -91,4 +91,16 @@ public class JunitUserRegistrationTest {
         boolean actualResult = junitUserRegistration.passwordRule3("jnsdjcvcp");
         Assert.assertEquals(false, actualResult);
     }
+
+    @Test
+    public void givenPasswordRule4_IsProper_ReturnTrue() {
+        boolean actualResult = junitUserRegistration.passwordRule4("qw98hRL@&dl");
+        Assert.assertEquals(true, actualResult);
+    }
+
+    @Test
+    public void givenPasswordRule4_IsNotProper_ReturnFalse() {
+        boolean actualResult = junitUserRegistration.passwordRule4("jsdjcvcp");
+        Assert.assertEquals(false, actualResult);
+    }
 }
