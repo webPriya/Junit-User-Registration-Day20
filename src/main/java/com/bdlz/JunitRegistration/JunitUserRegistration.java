ea5b683226a4fd918944756923982f9c04c1e9c5
webPriya <priyachouhan1019@gmail.com>
2021-10-28 17:59:27 -0700

    test case for password rule 1

--- src/main/java/com/bdlz/JunitRegistration/JunitUserRegistration.java
+++ src/main/java/com/bdlz/JunitRegistration/JunitUserRegistration.java
@@ -33,4 +33,11 @@ public class JunitUserRegistration {
         Matcher matcher = pattern.matcher(mobileNumber);
         return matcher.matches();
     }
+
+    public boolean passwordRule1(String password) {
+        regex = "^[a-z]{8,}$";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(password);
+        return matcher.matches();
+    }
 }
--- src/test/java/com/bdlz/JunitRegistration/JunitUserRegistrationTest.java
+++ src/test/java/com/bdlz/JunitRegistration/JunitUserRegistrationTest.java
@@ -55,4 +55,16 @@ public class JunitUserRegistrationTest {
         boolean actualResult = junitUserRegistration.mobileNumber("919989774456");
         Assert.assertEquals(false, actualResult);
     }
+
+    @Test
+    public void givenPasswordRule1_IsProper_ReturnTrue() {
+        boolean actualResult = junitUserRegistration.passwordRule1("qwgthjkdl");
+        Assert.assertEquals(true, actualResult);
+    }
+
+    @Test
+    public void givenPasswordRule1_IsNotProper_ReturnFalse() {
+        boolean actualResult = junitUserRegistration.passwordRule1("jnsdsjRcvc");
+        Assert.assertEquals(false, actualResult);
+    }
 }
